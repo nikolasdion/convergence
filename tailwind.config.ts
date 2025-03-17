2475831dcdf52f562b9d1919e2a5c430fa488b9f
Dion Susanto <nikolasdion@gmail.com>
2025-03-17 22:56:48 +0000

    Update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,7 @@ import type { Config } from "tailwindcss";
 export default {
   content: [
     "./index.html",
-    "./src/client/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx,mjs,mts}",
   ],
   theme: {
